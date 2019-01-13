1d8e4303b934eb8849c9219b7dafdda8921e3918
MKZ2018 <league@WorthyMini-6.attlocal.net>
2019-01-13 11:27:38 -0800

    saved

--- src/methods/Rollercoaster.java
+++ src/methods/Rollercoaster.java
@@ -1,5 +1,11 @@
 package methods;
 
+import javax.swing.JOptionPane;
+
 public class Rollercoaster {
+	public static void main(String[] args) {
+	String inches = JOptionPane.showInputDialog("How tall are you? (in)");
+		if(inches.equals(48)
+	}
 
 }
